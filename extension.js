41b463f77f208fe7752ed8c08c1fb3310052acbe
Vit Stanislav <slaweet@seznam.cz>
2012-01-03 09:10:13 +0100

    makes enable and disable easier

--- extension.js
+++ extension.js
@@ -8,11 +8,10 @@
  * duration.
  *
  */
-const Lang = imports.lang;
 const St = imports.gi.St;
-const Main = imports.ui.main;
-const PopupMenu = imports.ui.popupMenu;
+
 const Calendar = imports.ui.calendar;
+const Main = imports.ui.main;
 
 // in org.gnome.desktop.interface
 const CLOCK_FORMAT_KEY        = 'clock-format';
@@ -25,13 +24,14 @@ function _downToHalfHour(date) {
     return new Date(date.getTime() - date.getTime() % (30 * 60 * 1000));
 }
 
-
-function EventsTable(eventSource) {
-    this._init(eventSource);
+function EventsTableExtension() {
+    this._init();
 }
 
-EventsTable.prototype = {
-    __proto__: Calendar.EventsList.prototype,
+EventsTableExtension.prototype = {
+    _init: function() {
+        this.eventList = Main.panel._dateMenu._eventList;
+    },
 
     _addTableEvent: function(parentBox, event, clockFormat, widthRatio) {
         let time = Calendar._formatEventTime(event, clockFormat);
@@ -52,6 +52,7 @@ EventsTable.prototype = {
         descLabel.clutter_text.line_wrap = true;
         box.add(descLabel)
     },
+
     _widthRatio: function(events, n) {
             let first = (n == 0 ? 1 : 0);
             let count = (n == events.length -1 ? 1 : 2);
@@ -68,7 +69,7 @@ EventsTable.prototype = {
         let events = this._eventSource.getEvents(begin, end);
 
         if (events.length == 0 || includeDayName) {
-			Calendar.EventsList.prototype._addPeriod.call(this, header, begin, end, includeDayName, showNothingScheduled);
+			this._originalAddPeriod(header, begin, end, includeDayName, showNothingScheduled);
 			return;
 		}
         let clockFormat = this._desktopSettings.get_string(CLOCK_FORMAT_KEY);;
@@ -129,44 +130,27 @@ EventsTable.prototype = {
             }
 		}
         marginAllDayBox.set_width(timeBox.get_width());
-    }
-}
-
-
-
-
-function EventsTableExtension() {
-    this._init();
-}
-
-EventsTableExtension.prototype = {
-    _init: function() {
-        this.eventsTable = new EventsTable(Main.panel._dateMenu._eventSource);
-        this.eventsList = Main.panel._dateMenu._eventList;
-        this.eventBox = Main.panel._dateMenu.menu.box.get_children()[0].get_children()[2];
-    },
-
-    _update: function(events) {
-        Main.panel._dateMenu._eventList = events;
-        //Don't know how to insert eventList before Open Calendar, so destroy all and add in desired order.
-        this.eventBox.destroy_children();
-        this.eventBox.add_actor(Main.panel._dateMenu._eventList.actor,0);
-            
-        let item = new PopupMenu.PopupMenuItem(_("Open Calendar"));
-        item.connect('activate', Lang.bind(Main.panel._dateMenu, Main.panel._dateMenu._onOpenCalendarActivate));
-        item.actor.can_focus = false;
-        this.eventBox.add(item.actor, {y_align: St.Align.END, expand: true, y_fill: false}, 0);
     },
 
     disable: function() {
-        this._update(this.eventsList);
+        let list = this.eventList;
+        if(list._originalAddPeriod)
+            list._addPeriod = list._originalAddPeriod;
+        list._originalAddPeriod = undefined;
+        list._addTableEvent = undefined;
+        list._widthRatio = undefined;
     },
+
     enable: function() {
-        this._update(this.eventsTable);
+        let list = this.eventList;
+        if(!list._originalAddPeriod)
+            list._originalAddPeriod = list._addPeriod;
+        list._addPeriod = this._addPeriod;
+        list._addTableEvent = this._addTableEvent;
+        list._widthRatio = this._widthRatio;
     }	   
 }
 
 function init() {
     return new EventsTableExtension();
 }
-
